39db0f4d1fef8e98826f0c2921a377018cfdc22d
valeria <valeria.penaloza5@gmail.com>
2018-09-25 12:56:00 -0300

    modificaciones

---
+++ docs/js/app.js
@@ -0,0 +1,26 @@
+// $(document).ready(function() {
+
+//     var bloques = $('.bloque'),
+//         enlaces = $('#navegacion').find('a');
+
+//     enlaces.click(function(e) {
+
+//         e.preventDefault();
+
+//         var hash = $(this).attr('href');
+
+//         bloques
+//             .filter(hash)
+//             .addClass('visible')
+//             .siblings()
+//             .removeClass('visible');
+
+//         enlaces
+//             .removeClass('activo');
+
+//         $(this)
+//             .addClass('activo');
+
+//     });
+
+// });
