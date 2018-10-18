d1e24da3e737224a60bfdcc94788a529add2c055
valeria <valeria.penaloza5@gmail.com>
2018-10-18 11:42:01 -0300

    probando soluciones fondo

--- docs/js/app.js
+++
@@ -1,7 +0,0 @@
-// window.onload = () => {
-//     setTimeout(() => {
-//         const containerPreloader = document.getElementById('containerPreloader');
-//         containerPreloader.style.visibility = 'hidden';
-//         containerPreloader.style.opacity = '0';
-//     }, 2000)
-// };
